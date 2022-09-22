24d156e0cc4926f25467f06e677404af4bdf8528
PAN zy <zypan@ihep.ac.cn>
2022-09-22 12:29:51 +0800

    test theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,8 +32,8 @@ extensions = [
 # templates_path = ['_templates']
 
 # -- Options for HTML output
-
-html_theme = 'sphinx_rtd_theme'
+import pydata_sphinx_theme
+html_theme = 'pydata_sphinx_theme'
 
 # -- Options for EPUB output
 # epub_show_urls = 'footnote'
