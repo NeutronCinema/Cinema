bc315c346d91fe47f64972f534b9e3719efca3d8
PAN zy <zypan@ihep.ac.cn>
2022-10-14 16:28:16 +0800

    fix syntax bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ imgmath_use_preview = True
 # if png format is required
 # imgmath_dvipng_args = ['-gamma', '1.5', '-D', '110', '-bg', 'Transparent']
 
-intersphinx_mapping = {
+# intersphinx_mapping = {
 #     'python': ('https://docs.python.org/3/', None),
 #     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 # }
