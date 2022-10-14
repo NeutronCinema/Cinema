3a038aa6c22466ef9b57db9f98489717aaf5e4e9
PAN zy <zypan@ihep.ac.cn>
2022-10-14 15:59:37 +0800

    add current supported geometries

--- docs/source/_utils/cinema_plot.py
+++ docs/source/_utils/cinema_plot.py
@@ -1,37 +1,39 @@
 
-def tutorial_plot(fn):
+def tutorial(fn):
     
     import os, sys
     import pyvista
     import stdout_redirect as rd
     from contextlib import redirect_stdout
 
+    # configure global theme
+    # panel is the only jupyter backend working good
+    # a new backend might be avaible in the future,
+    # see https://github.com/pyvista/pyvista/issues/3348#issuecomment-1255282449
     pyvista.set_jupyter_backend('panel')
-    pyvista.global_theme.background = 'grey'
-    # pyvista.global_theme.window_size = [300, 200]
+    pyvista.global_theme.background = '#f3f6f6'
     
-    import Cinema
     from Cinema.Prompt import Launcher, Visualiser
-    import matplotlib.pyplot as plt
-    import numpy as np
     from Cinema.Interface.Utils import findData
-
-    printTraj=False
-    inputfile = os.path.join(os.environ["CDOCGDML"], 'volumes', fn)
+    
+    if os.environ['CDOCGDML']:
+        try:
+            inputfile = os.path.join(os.environ["CDOCGDML"], 'volumes', fn)
+        except:
+            raise OSError(f'file not found: {inputfile}')
+    else:
+        raise OSError(f'Check environment variable CDOCGDML:{os.environ["CDOCGDML"]}')
 
     if not os.path.isfile(inputfile):
         raise IOError('gdml not found: ' + inputfile)
+        
     with rd.jupyter2terminal():
         with open('output.txt', 'w') as fto:
             with rd.stdout_redirected(fto, sys.__stdout__):
                 with redirect_stdout(fto):
                     myLcher=Launcher()
                     myLcher.loadGeometry(inputfile)
-
-                    visualize = True
-
-                    if visualize is True:
-                        v = Visualiser('+', printWorld=False)
-                        v.plotter.show_bounds()
-                        v.plotter.show_grid()
-                        v.plotter.show()
+                    v = Visualiser('+', printWorld=False)
+                    v.plotter.show_bounds()
+                    v.plotter.show_axes()
+                    v.plotter.show()
