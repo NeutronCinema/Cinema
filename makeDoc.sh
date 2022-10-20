cf38f64e78c4e4ad82206aeae70ee9a334936260
PAN zy <zypan@ihep.ac.cn>
2022-10-20 16:10:06 +0800

    add conda environment config

--- makeDoc.sh
+++ makeDoc.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+if { conda env list | grep 'cinema_doc'; } >/dev/null 2>&1
+then conda activate cinema_doc
+else conda env create -f environmentDoc.yml && conda activate cinema_doc
+fi
+
 cd docs/
 rm -rf build
 rm -rf source/_doxygen
 rm -rf source/api
 
 make html
-cd ../
+cd ..
