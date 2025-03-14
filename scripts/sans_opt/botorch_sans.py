4ae1bfc38b1d9741cd2f69851936e35b6e59d1e5
panzy <zypan@ihep.ac.cn>
2025-03-14 02:05:19 +0000

    draft sans scripts

--- scripts/sans_opt/botorch_sans.py
+++ scripts/sans_opt/botorch_sans.py
@@ -3,7 +3,7 @@
 from Cinema.Prompt import Prompt, PromptMPI
 from Cinema.Prompt.geo import Volume, Transformation3D
 from Cinema.Prompt.solid import Box,Tube
-from Cinema.Prompt.scorer import makePSD, ESpectrumHelper, WlSpectrumHelper, TOFHelper, VolFluenceHelper, PSDHelper, DirectSqwHelper, KillMCPLHelper
+from Cinema.Prompt.scorer import makePSD, ESpectrumHelper,MultiScatCounter, WlSpectrumHelper, TOFHelper, VolFluenceHelper, PSDHelper, DirectSqwHelper, KillMCPLHelper
 from Cinema.Prompt.gun import PythonGun, SimpleThermalGun, MaxwellianGun
 from Cinema.Prompt.histogram import wl2ekin
 from Cinema.Prompt.physics import Material, Mirror
@@ -22,38 +22,50 @@ class MySim(PromptMPI):
     def __init__(self, seed=4096) -> None:
         super().__init__(seed)  
 
-    def makeWorld(self, wl, t):
-        matCfg_sample = Material('LiquidWaterH2O_T293.6K.ncmat')
+    def makeWorld(self, wl, t, detpos):
+        # matCfg_sample = Material('LiquidWaterH2O_T293.6K.ncmat')
+        # matCfg_sample = Material('Al2O3_sg167_Corundum.ncmat')
         # matCfg_sample = Material('PTWaterH2O_T293.6K.ncmat')
 
         # matCfg_sample = Material('PTHeavyWater_T293.6K.ncmat;ucnmode=refine:0.01eV')
         # matCfg_sample = Material('PTHeavyWater_T293.6K.ncmat')
 
-        # matCfg_sample = Material('LiquidHeavyWaterD2O_T293.6K.ncmat')
+        # matCfg_sample = Material('LiquidHeavyWaterD2O_T293.6K.ncmat')V_sg229.ncmat
+        matCfg_sample = Material('nanodiamond.ncmat')
+        matCfg_sample.setBiasScat(2.0)
        
         world = Volume("world", Box(10000, 10000, 25000))
         sample = Volume('sample', Box(10, 10, t), matCfg = matCfg_sample)
         detector = Volume("det", Tube(beamstop_radius_mm, det_radius_mm, 0.0001))
-
+        ms = MultiScatCounter()
+        ms.make(sample)
         world.placeChild("sample", sample, Transformation3D(0., 0., 0))
         world.placeChild("det", detector, Transformation3D(0., 0., det_pos))
 
-        PSDHelper('psd', -det_radius_mm, det_radius_mm, 100,  -det_radius_mm, det_radius_mm, 100).make(detector)
+        helper = PSDHelper('psd', -det_radius_mm, det_radius_mm, 100,  -det_radius_mm, det_radius_mm, 100)
+        helper.make(detector)
+        helper.addScatterCounter(ms, 1)
+
+        helper2 = PSDHelper('psd2', -det_radius_mm, det_radius_mm, 100,  -det_radius_mm, det_radius_mm, 100)
+        helper2.make(detector)
+
         ESpectrumHelper('espec').make(detector)
         WlSpectrumHelper('wlspec').make(detector)
         TOFHelper('tof', max=50e-3).make(detector)
         DirectSqwHelper('sqw', mod_sam_dist, wl2ekin(wl), sample_position=sam_pos, qmin=1e-1, 
-                        qmax=30, num_qbin=100, ekinmin=-1, ekinmax=1, num_ebin=1001 ).make(detector)
-        DirectSqwHelper('sqw_s', mod_sam_dist, wl2ekin(wl), sample_position=sam_pos, qmin=1e-3, 
-                        qmax=.5, num_qbin=1000, ekinmin=-.01, ekinmax=.01, num_ebin=101, logx=True ).make(detector)
-        self.kill = KillMCPLHelper('part_gen', 2112)
-        self.kill.make(detector)
+                        qmax=5, num_qbin=100, ekinmin=-0.1, ekinmax=0.1, num_ebin=110 ).make(detector)
+        helpersqw = DirectSqwHelper('sqw_s', mod_sam_dist, wl2ekin(wl), sample_position=sam_pos, qmin=1e-3, 
+                        qmax=.1, num_qbin=20, ekinmin=-.01, ekinmax=.01, num_ebin=101, logx=True )
+        helpersqw.linear=False
+        helpersqw.make(detector)
+        # self.kill = KillMCPLHelper('part_gen', 2112)
+        # self.kill.make(detector)
 
         self.setWorld(world)
 
 sim = MySim(seed=1010)
 
-def sans_run(wl, n, t, pyGun=False, sim=sim):
+def sans_run(wl, n, t, det_pos, divergence, pyGun=True, sim=sim):
 
     class MyGun(PythonGun):
         def __init__(self, pdg):
@@ -66,9 +78,11 @@ def sans_run(wl, n, t, pyGun=False, sim=sim):
             return np.array(gun_pos)
         
         def sampleDirection(self):
-            return np.array([0, 0, 1])
+            x = np.random.random() * divergence / mod_sam_dist
+            y = np.random.random() * divergence / mod_sam_dist
+            return np.array([x, y, 1])
 
-    sim.makeWorld(wl,t)
+    sim.makeWorld(wl,t,detpos=det_pos)
 
     usePythonGun=pyGun
 
@@ -88,30 +102,78 @@ def sans_run(wl, n, t, pyGun=False, sim=sim):
     sqw = sim.gatherHistData('sqw')
     sqw_s = sim.gatherHistData('sqw_s')
 
-    psd = sim.gatherHistData('psd')
+    psd = sim.gatherHistData('psd') # 1 scatter
+    psd2 = sim.gatherHistData('psd2') # all
     espec = sim.gatherHistData('espec')
     wlspec = sim.gatherHistData('wlspec')
     tof = sim.gatherHistData('tof')
-    res = psd.getAccWeight()
-    return sim, res
-
-# if sim.rank==0: 
-#     psd.save('psd.h5')
-#     espec.save('espec.h5')
-#     wlspec.save('wlspec.h5')
-#     tof.save('tof.h5')
-#     sqw.save('sqw.h5')
-#     sqw_s.save('sqw_s.h5')
-#     # sqw.savefig('sqw.pdf', log=True)
+    all = psd2.getAccWeight()
+    signal = psd.getAccWeight()
+    res = signal
+    return sim, sqw_s
 
 
-#     # psd.plot(show=False)
-#     sqw.plot(show=False, dynrange=1e-10)
-    # sqw_s.plot(show=False, dynrange=1e-10, logx=True)
-    # plt.figure()
+if __name__ == "__main__":
+    class MyGun(PythonGun):
+        def __init__(self, pdg):
+            super().__init__(pdg)
 
-    # espec.plot(show=False, log=True)
-    # plt.figure()
-    # wlspec.plot(show=False,  log=[False, True])
-    # plt.figure()
-    # tof.plot(show=True)
+        def sampleEnergy(self):
+            wl = np.random.random() * 3 + 1
+            return wl2ekin(wl)
+
+        def samplePosition(self):
+            return np.array(gun_pos)
+        
+        def sampleDirection(self):
+            return np.array([0, 0, 1])
+    wl = 1
+    t = 10
+    numNeutron = 1e8
+    usePythonGun = True
+    sim.makeWorld(wl,t,detpos=det_pos)
+    if usePythonGun:
+        gun = MyGun(2112)
+    else:
+        gun = SimpleThermalGun()
+        gun.setPosition(gun_pos) 
+        gun.setDirection([0, 0, 1])
+        gun.setWavelength(wl)
+    if False:
+        sim.show(gun, 100)
+    else:
+        sim.simulate(gun, numNeutron)
+    sqw = sim.gatherHistData('sqw')
+    sqw_s = sim.gatherHistData('sqw_s')
+
+    psd = sim.gatherHistData('psd') # 1 scatter
+    psd2 = sim.gatherHistData('psd2') # all
+    espec = sim.gatherHistData('espec')
+    wlspec = sim.gatherHistData('wlspec')
+    tof = sim.gatherHistData('tof')
+    all = psd2.getAccWeight()
+    signal = psd.getAccWeight()
+    if sim.rank==0: 
+    #     psd.save('psd.h5')
+    #     espec.save('espec.h5')
+    #     wlspec.save('wlspec.h5')
+    #     tof.save('tof.h5')
+    #     sqw.save('sqw.h5')
+    #     sqw_s.save('sqw_s.h5')
+        sq = sqw.getWeight().sum(1)
+        q = sqw.getCentre()[0]
+        fig = plt.figure(0)
+        plt.plot(q,sq)
+        fig.savefig('sq.pdf')
+        sqw.savefig('sqw.pdf', log=True)
+
+
+        # psd.plot(show=False)
+        # sqw.plot(show=False, dynrange=1e-10)
+        # sqw_s.plot(show=False, dynrange=1e-10, logx=True)
+        # plt.figure()
+        # espec.plot(show=False, log=True)
+        # plt.figure()
+        # wlspec.plot(show=False,  log=[False, True])
+        # plt.figure()
+        # tof.plot(show=True)
--- scripts/sans_opt/run_generate.py
+++ scripts/sans_opt/run_generate.py
@@ -3,14 +3,37 @@
 import numpy as np
 from botorch_sans import sans_run
 
-numNeutron = 1e6
-x1 = np.linspace(1e-1, 10, 20)
-y = []
-for t in x1:
-    sim, yy = sans_run(1,numNeutron,t)
-    sim.clear()
-    y.append(yy)
+import multiprocessing
 
-result = np.vstack((x1, np.array(y)))
-print(result)
-np.savetxt('data.csv', result.T, delimiter=',')
+numNeutron = 1e6
+# thickness = np.linspace(0.1, 10, 3)
+thickness = np.array([0.1,2,5,10])
+detpos = np.linspace(1100, 25000, 30)
+divergence = np.linspace(0, 5, 30)
+wl = np.linspace(1, 6, 5)
+var = thickness
+
+def process(x):
+    sim, sqw = sans_run(3, numNeutron, x, 6000, 0)
+    qq = sqw.getCentre()[0]
+    sq = sqw.getWeight().sum(1)/sqw.getAccWeight()
+    sim.clear()
+    return (qq,sq)
+
+# for t in x1:
+#     sim, yy = sans_run(1,numNeutron,t)
+#     sim.clear()
+#     y.append(yy)
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    plt.figure(0)
+    with multiprocessing.Pool() as pool:
+        sqw = pool.map(process, var)
+        zzip = list(zip(sqw,var))
+        base = zzip.pop(0)
+        for sqw_s, v in zzip:
+            plt.plot(sqw_s[0], sqw_s[1]/base[0][1], label=f'{v}')
+        plt.legend()
+        plt.xscale("log")
+        plt.savefig('thickness.pdf')
