a2bc3802e523b83591213d81ac87bc4d8e610506
zypan <zypan@ihep.ac.cn>
2025-11-25 10:41:05 +0800

    fix due to #40 and !3

--- src/pythontests/test_prompt_scorer.py
+++ src/pythontests/test_prompt_scorer.py
@@ -7,13 +7,13 @@ from Cinema.Prompt import PromptFileReader
 scorertest_dict ={}
 scorertest_dict['ESpectrum'] = {'gdml': 'ESpectrum.gdml', 'mcpl': 'ScorerESpectrum_detector_seed113.mcpl.gz', 'value': [ 0.335562367659022, 88., 88., 1.8522594945025759]}
 scorertest_dict['WlSpectrum'] = {'gdml': 'WlSpectrum.gdml', 'mcpl': 'ScorerWlSpectrum_detector_seed113.mcpl.gz', 'value': [5050.,813.,813.,832.]}
-scorertest_dict['MultiScat'] = {'gdml': 'MultiScat.gdml', 'mcpl': 'ScorerMultiScat_D2O_seed113.mcpl.gz', 'value': [ 18., 368., 368., 260.]}
+scorertest_dict['MultiScat'] = {'gdml': 'MultiScat.gdml', 'mcpl': 'ScorerMultiScat_D2O_seed113.mcpl.gz', 'value': [ 18., 444., 444., 315.]}
 scorertest_dict['DeltaMomentum'] = {'gdml': 'DeltaMomentum.gdml', 'mcpl': 'ScorerDeltaMomentum_SofQ_seed113.mcpl.gz', 'value': [25275.25, 63.69149327544164, 190., 188.4447368017572 ]}
 scorertest_dict['Angular'] = {'gdml': 'Angular.gdml', 'mcpl': 'ScorerAngular_SofAngle_seed113.mcpl.gz', 'value': [30030. ,   743.  ,   743.  ,  5011.98]}
 # fixme
 # scorertest_dict['RotatingObj'] = {'gdml': 'RotatingObj.gdml', 'mcpl': 'ScorerRotatingObj_roen_seed113.mcpl.gz', 'value': [  50.5 , 1926.  , 1926.  ,   44.87]}
 scorertest_dict['TOF'] = {'gdml': 'TOF.gdml', 'mcpl': 'ScorerTOF_detector_seed113.mcpl.gz', 'value': [250.25, 813.,813.,0.]}
-scorertest_dict['VolFluence'] = {'gdml': 'VolFluence.gdml', 'mcpl': 'ScorerVolFluence_Sflux_seed113.mcpl.gz', 'value': [6.060050500000000e+01, 5.941708706493470e-03, 1.672300000000000e+04, 2.646847618584686e-04]}
+scorertest_dict['VolFluence'] = {'gdml': 'VolFluence.gdml', 'mcpl': 'ScorerVolFluence_Sflux_seed113.mcpl.gz', 'value': [6.0600504999999998e+01, 5.6186115051861350e-03, 1.5993000000000000e+04, 2.4906084586065675e-04]}
 scorertest_dict['PSD'] = {'gdml': 'PSD.gdml', 'mcpl': 'ScorerPSD_NeutronHistMap_seed113.mcpl.gz', 'value': [  0.,0., 805., 805., 120., 120.]}
 scorertest_dict['guide'] = {'gdml': 'guide.gdml', 'mcpl': 'ScorerPSD_Monitor2_seed113.mcpl.gz', 'value': [  0.,0.,194.7600562359724, 211.,95.60145860572976,95.60145860572976]
 }
