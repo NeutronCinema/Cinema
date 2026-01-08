478a96596c8eb5115b0d3739f897b2394e57adbb
zypan <zypan@ihep.ac.cn>
2026-01-08 14:45:58 +0800

    remove light write color

--- src/python/Cinema/Prompt/Visualiser.py
+++ src/python/Cinema/Prompt/Visualiser.py
@@ -36,7 +36,7 @@ except Exception as e:
 high_contrast_colors = ["#FF0000","#FF4500","#FF8C00","#FFD700","#FFFF00","#FF6347","#CD5C5C",
     "#0000FF","#1E90FF","#00BFFF","#00FFFF","#00FF00","#32CD32","#2E8B57","#20B2AA",
     "#800080","#9370DB","#8A2BE2","#DA70D6","#FF00FF","#00FF7F","#4B0082","#7FFF00","#FF1493",
-    "#9400D3","#000000","#FFFFFF","#696969","#A9A9A9","#D3D3D3" ]
+    "#9400D3","#000000","#696969","#A9A9A9"]
 
 # from https://stackoverflow.com/questions/57173235/how-to-detect-whether-in-jupyter-notebook-or-lab 
 def is_jupyterlab_session() -> bool:
